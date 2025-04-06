f3cae2a3c2865128be6cabc516a82c03368a44c0
vinyferrero <vinyferrero.kl@gmail.com>
2025-04-06 18:52:57 +0800

    update

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import redis
+import time
 
 # Connect to Redis server
 r = redis.Redis(host='localhost', port=6380, db=0)
@@ -12,16 +13,21 @@ pubsub = r.pubsub()
 pubsub.subscribe(ping_channel)
 
 print("Listening for ping messages...")
+try:
+    for message in pubsub.listen():
+        if message['type'] == 'message':
+            ping_message = message['data'].decode('utf-8')
+            print(f"Received: {ping_message}")
 
-for message in pubsub.listen():
-    if message['type'] == 'message':
-        ping_message = message['data'].decode('utf-8')
-        print(f"Received: {ping_message}")
+            # Extract the ping counter value
+            ping_counter = ping_message.split()[1]
 
-        # Extract the ping counter value
-        ping_counter = ping_message.split()[1]
-
-        # Publish the pong message to the pong channel
-        pong_message = f"pong {ping_counter}"
-        r.publish(pong_channel, pong_message)
-        print(f"Sent: {pong_message}")
+            # Publish the pong message to the pong channel
+            pong_message = f"pong {ping_counter}"
+            r.publish(pong_channel, pong_message)
+            print(f"Sent: {pong_message}")
+except Exception as e:
+    print(f"An exception raised => {e}")
+    while True:
+        time.sleep(5)
+        print("Run but DEAD!!!")
